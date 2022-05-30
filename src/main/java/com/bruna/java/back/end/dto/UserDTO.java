fc339d839f8efa177573d6ec2592b66dbcb282f6
Bruna Neto Bitencourt <brunaneto.b@gmail.com>
2022-05-30 17:08:37 -0300

    Inseridas dependencias para integração com BD

--- src/main/java/com/bruna/java/back/end/dto/UserDTO.java
+++ src/main/java/com/bruna/java/back/end/dto/UserDTO.java
@@ -2,6 +2,8 @@ package com.bruna.java.back.end.dto;
 
 import java.util.Date;
 
+import com.bruna.java.back.end.domain.User;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,5 +18,16 @@ public class UserDTO {
 	private String email;
 	private String telefone;
 	private Date dataCadastramento;
+	
+	public static UserDTO convert(User user) {
+		UserDTO userDTO = new UserDTO();
+		userDTO.setNome(user.getNome());
+		userDTO.setEndereco(user.getEndereco());
+		userDTO.setCpf(user.getCpf());
+		userDTO.setEmail(user.getEmail());
+		userDTO.setTelefone(user.getTelefone());
+		userDTO.setDataCadastramento(user.getDataCadastramento());
+		return userDTO;
+	}
 
 }
