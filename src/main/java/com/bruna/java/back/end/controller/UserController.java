ff4fe22eec3f9371829f891ae5f0d84865bf071d
Bruna Neto Bitencourt <brunaneto.b@gmail.com>
2022-06-11 17:11:04 -0300

    Adequação no controller para chamar a service

--- src/main/java/com/bruna/java/back/end/controller/UserController.java
+++ src/main/java/com/bruna/java/back/end/controller/UserController.java
@@ -1,12 +1,8 @@
 package com.bruna.java.back.end.controller;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,77 +10,40 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bruna.java.back.end.dto.UserDTO;
+import com.bruna.java.back.end.service.UserService;
 
 @RestController
 public class UserController {
-	public static List<UserDTO> usuarios = new ArrayList<UserDTO>();
+	
+	@Autowired
+	UserService userService;
+	
 	
 	@GetMapping("/")
 	public String getMensagem() {
 		return "Spring boot is working!";
 	}
 	
-	@GetMapping("/users")
+	@GetMapping("/user")
 	public List<UserDTO> getUsers() {
-		return usuarios;
+		return userService.getAll();
+	
 	}
 	
-	@GetMapping("/users/{cpf}")
-	public UserDTO getUsersFiltro(@PathVariable String cpf) {
-			for(UserDTO userFilter:usuarios) {
-				if(userFilter.getCpf().equals(cpf)) {
-					return userFilter;
-				}
-			}
-			return null;
+	@GetMapping("/user/{id}")
+	UserDTO findById(@PathVariable long id) {
+		return userService.findById(id);
 	}
 	
-	@PostMapping("/newUser")
-	public UserDTO inserir(@RequestBody UserDTO newUser) {
-		newUser.setDataCadastramento(new Date());
-		usuarios.add(newUser);
-		return newUser;		
+	@PostMapping("/user")
+	UserDTO newUser(@RequestBody UserDTO userDTO) {
+		return userService.save(userDTO);
 	}
 	
-	@DeleteMapping("/users/{cpf}")
-	public boolean deleteUserFiltro(@PathVariable String cpf) {
-			for(UserDTO userFilter:usuarios) {
-				if(userFilter.getCpf().equals(cpf)) {
-					usuarios.remove(userFilter);
-					return true;
-				}
-			}
-			return false;
-	}	
-	
-	@PostConstruct //@PostConstruct faz com que este método seja executado logo depois que o container inicializa a classe UserController.
-	public void initiateList() {
-		UserDTO userDto1 = new UserDTO();
-		userDto1.setNome("Eduardo");
-		userDto1.setCpf("13729004034");
-		userDto1.setEndereco("Rua a exemplo");
-		userDto1.setEmail("eduardo@email.com");
-		userDto1.setTelefone("790973981");
-		userDto1.setDataCadastramento(new Date());
-		
-		UserDTO userDto2 = new UserDTO();
-		userDto2.setNome("Luiz");
-		userDto2.setCpf("58594208030");
-		userDto2.setEndereco("Rua b exemplo");
-		userDto2.setEmail("luiz@email.com");
-		userDto2.setTelefone("790973981");
-		userDto2.setDataCadastramento(new Date());
-		
-		UserDTO userDto3 = new UserDTO();
-		userDto3.setNome("Bruna");
-		userDto3.setCpf("53378045027");
-		userDto3.setEndereco("Rua c exemplo");
-		userDto3.setEmail("bruna@email.com");
-		userDto3.setTelefone("790973981");
-		userDto3.setDataCadastramento(new Date());
-		
-		usuarios.add(userDto1);
-		usuarios.add(userDto2);
-		usuarios.add(userDto3);
+	@GetMapping("/user/cpf/{cpf}")
+	UserDTO findByCpf(@PathVariable String cpf) {
+		return userService.findByCpf(cpf);
 	}
+	
+	
 }
